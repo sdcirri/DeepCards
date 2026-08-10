db50aa49621225927b85fae222dfc596ff0a5dcc
sdcirri <sebadeod@proton.me>
2026-08-10 21:40:20 +0200

    parallelization

--- agents/tressette/agent.py
+++ agents/tressette/agent.py
@@ -1,5 +1,7 @@
+from concurrent.futures import ProcessPoolExecutor
 from gymnasium.utils import seeding
 from abc import abstractmethod, ABC
+import os
 
 from environments.tressette_env import Tressette2PEnv, AgentId, Action, env as env_factory
 
@@ -27,15 +29,21 @@ def challenge_step(env: Tressette2PEnv, agent1: Tressette2PAgent, agent2: Tresse
             env.step(agent2.step(env))
 
 
+def _one_episode(a1: Tressette2PAgent, a2: Tressette2PAgent) -> tuple[int, int]:
+    env = env_factory(render_mode=None)
+    challenge_step(env, a1, a2)
+    raw = env.unwrapped
+    # Points are stored in thirds, score is truncated at the end of the game
+    return raw.scores['p1'] // 3, raw.scores['p2'] // 3
+
+
 def challenge(agent1: Tressette2PAgent, agent2: Tressette2PAgent, episodes: int) -> tuple[int, int]:
+    with (ProcessPoolExecutor(max_workers=os.cpu_count()) as executor):
+        results = executor.map(_one_episode, [agent1] * episodes, [agent2] * episodes)
+
     points_1, points_2 = 0, 0
-
-    for _ in range(episodes):
-        env = env_factory(render_mode=None)
-        challenge_step(env, agent1, agent2)
-
-        # Points are stored in thirds, score is truncated at the end of the game
-        points_1 += env.scores['p1'] // 3
-        points_2 += env.scores['p2'] // 3
+    for result in results:
+        points_1 += result[0]
+        points_2 += result[1]
 
     return points_1, points_2
